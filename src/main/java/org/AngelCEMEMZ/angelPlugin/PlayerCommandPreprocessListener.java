50cf06a732b48c6a04dd859825bbb5ca197ce5aa
Luke Zhang <admin@lukezhang.win>
2025-01-08 10:05:58 +0800

    Fix: Update Invisible Player Protection

--- src/main/java/org/AngelCEMEMZ/angelPlugin/PlayerCommandPreprocessListener.java
+++ src/main/java/org/AngelCEMEMZ/angelPlugin/PlayerCommandPreprocessListener.java
@@ -1,3 +1,19 @@
+///     AngelPlugin - A Minecraft plugin that adds useful commands and features.
+///     Copyright (C) 2025 Luke Zhang & AngelCEMEMZ
+///
+///     This program is free software: you can redistribute it and/or modify
+///     it under the terms of the GNU General Public License as published by
+///     the Free Software Foundation, either version 3 of the License, or
+///     (at your option) any later version.
+///
+///     This program is distributed in the hope that it will be useful,
+///     but WITHOUT ANY WARRANTY; without even the implied warranty of
+///     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///     GNU General Public License for more details.
+///
+///     You should have received a copy of the GNU General Public License
+///     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package org.AngelCEMEMZ.angelPlugin;
 
 import org.bukkit.Bukkit;
@@ -15,12 +31,17 @@ public class PlayerCommandPreprocessListener implements Listener {
 
     @EventHandler
     public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
-        String[] args = event.getMessage().split(" ");
-        if (args.length > 1) {
-            Player targetPlayer = Bukkit.getPlayer(args[1]);
-            if (targetPlayer != null && visibilityManager.isInvisible(targetPlayer)) {
-                event.setCancelled(true);
-                event.getPlayer().sendMessage("You cannot interact with an invisible player.");
+        String message = event.getMessage();
+        String[] args = message.split(" ");
+
+        // 处理 say 和 kill 等命令
+        if ((args[0].equalsIgnoreCase("/say") || args[0].equalsIgnoreCase("/kill")) && args.length > 1) {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                if (visibilityManager.isInvisible(player) && message.contains(player.getName())) {
+                    event.setCancelled(true);
+                    event.getPlayer().sendMessage("You cannot interact with an invisible player.");
+                    return;
+                }
             }
         }
     }
